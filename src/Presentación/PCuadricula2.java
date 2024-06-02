27ea833ba740e48d8bd303035c9ba38451cad86c
AllenC <allenstivenc@gmail.com>
2024-06-02 15:59:31 -0500

    Version preliminar

--- src/Presentación/PCuadricula2.java
+++ src/Presentación/PCuadricula2.java
@@ -1,51 +1,135 @@
 package Presentación;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import Lógica.Movimiento;
-
-public class PCuadricula2 extends JPanel{
+public class PCuadricula2 extends JPanel {
 	Color cSnake = Color.green;
 	Color cManzana = Color.red;
-	private Movimiento movimiento;
-	List<int[]>Snake = new ArrayList<int[]>();
-	int[]comida;
-	int tam_max,tam,cant,res;
+	public List<int[]> Snake = new ArrayList<int[]>();
+	int[] comida;
+	int tam_max, tam, cant, res;
+	public String movimiento = "ARR";
+	public boolean juegoActivo = true;
+	private int puntos = 0;
+	private int bpuntos = 0;
 
-	public PCuadricula2(int tam_max, int cant, Movimiento movimiento) {
+	public PCuadricula2(int tam_max, int cant) {
 		this.tam_max = tam_max;
 		this.cant = cant;
-		this.tam = tam_max/cant;
-		this.res = tam_max%cant;
-		this.movimiento = movimiento;
-		int a[] = {cant/2-1, cant/2-1};
-		int b[] = {cant/2,cant/2-1};
+		this.tam = tam_max / cant;
+		this.res = tam_max % cant;
+		int a[] = {cant / 2 - 1, cant / 2 - 1};
+		int b[] = {cant / 2, cant / 2 - 1};
 		Snake.add(a);
 		Snake.add(b);
+
+		generarComida();
+
+		setFocusable(true);
+		requestFocusInWindow();
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				try {
+					teclaOprimida(e);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+	}
+
+	private void generarComida() {
+		comida = new int[]{(int) (Math.random() * cant), (int) (Math.random() * cant)};
 	}
 
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
 		g.setColor(cSnake);
-		for(int[] i : Snake) {
-			g.fillRect(res/2+i[0]*tam, res/2+i[1]*tam, tam-1, tam-1);	
+		for (int[] i : Snake) {
+			g.fillRect(res / 2 + i[0] * tam, res / 2 + i[1] * tam, tam - 1, tam - 1);
 		}
 		g.setColor(cManzana);
-        int[] comida = movimiento.getComida();
-        g.fillRect(res / 2 + comida[0] * tam, res / 2 + comida[1] * tam, tam - 1, tam - 1);
+		g.fillRect(res / 2 + comida[0] * tam, res / 2 + comida[1] * tam, tam - 1, tam - 1);
 	}
-	
-	public void actualizar() throws Exception {
-        movimiento.mover();
-        repaint();
-    }
-}
 
+	public void actualizar() throws Exception {
+		if (juegoActivo) {
+			moverSerpiente();
+			repaint();
+		}
+	}
+
+	private void moverSerpiente() {
+		int[] cabeza = Snake.get(0).clone();
+		if (movimiento.equals("ARR")) {
+			cabeza[1]--;
+		} else if (movimiento.equals("ABA")) {
+			cabeza[1]++;
+		} else if (movimiento.equals("IZQ")) {
+			cabeza[0]--;
+		} else if (movimiento.equals("DER")) {
+			cabeza[0]++;
+		}
+		Snake.add(0, cabeza);
+		if (cabeza[0] == comida[0] && cabeza[1] == comida[1]) {
+			generarComida();
+			puntos ++;
+		} else {
+			Snake.remove(Snake.size() - 1);
+		}
+		perder(cabeza);
+	}
+
+	private boolean chocaCuerpo(int[] cabeza) {
+		for (int i = 1; i < Snake.size(); i++) {
+			if (Snake.get(i)[0] == cabeza[0] && Snake.get(i)[1] == cabeza[1]) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void perder(int[] cabeza) {
+		if (cabeza[0] < 0 || cabeza[0] >= cant || cabeza[1] < 0 || cabeza[1] >= cant || chocaCuerpo(cabeza)) {
+			juegoActivo = false;
+			if(puntos > bpuntos) {
+				bpuntos = puntos;
+			}
+			JOptionPane.showMessageDialog(this, "Perdiste");
+		}
+	}
+
+	private void teclaOprimida(KeyEvent e) throws Exception {
+		if (!juegoActivo) {
+			return;
+		}
+		int keyCode = e.getKeyCode();
+		if (keyCode == KeyEvent.VK_LEFT && !movimiento.equals("DER")) {
+			movimiento = "IZQ";
+		} else if (keyCode == KeyEvent.VK_UP && !movimiento.equals("ABA")) {
+			movimiento = "ARR";
+		} else if (keyCode == KeyEvent.VK_RIGHT && !movimiento.equals("IZQ")) {
+			movimiento = "DER";
+		} else if (keyCode == KeyEvent.VK_DOWN && !movimiento.equals("ARR")) {
+			movimiento = "ABA";
+		}
+	}
+
+	public int getPuntos() {
+		return puntos;
+	}
+
+	public int getBpuntos() {
+		return bpuntos;
+	}
+}
--- src/Presentación/Visual.java
+++ src/Presentación/Visual.java
@@ -4,130 +4,95 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import Lógica.Movimiento;
-
 public class Visual extends JFrame {
-	private Movimiento movimiento;
-	private JPanel pElementos;
-	private JPanel pFondo;
-	private PCuadricula pCuadricula;
-	private JButton bNuevoJuego;
-	private JLabel lMejor_puntaje;
-	private JLabel lPuntos;
-	private PCuadricula2 pCuadricula2;
-	
-	public final static int IZQ = 37;
-	public final static int ARR = 38;
-	public final static int DER = 39;
-	public final static int ABA = 40;
-	
-	public Visual() {
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setSize(700, 700);
-		this.setTitle("Snake");
-		this.setLayout(new BorderLayout());
+    private JPanel pElementos;
+    private JPanel pFondo;
+    private PCuadricula pCuadricula;
+    private JButton bNuevoJuego;
+    private JLabel lMejor_puntaje;
+    private JLabel lPuntos;
+    private PCuadricula2 pCuadricula2;
 
-		// Panel de elementos
-		this.bNuevoJuego = new JButton("Nuevo Juego");
-		this.lMejor_puntaje = new JLabel("Mejor puntaje: 0");
-		this.lPuntos = new JLabel("Puntos: 0");
-		this.pElementos = new JPanel();
-		this.pElementos.setLayout(new GridLayout(1, 3, 10, 10));
-		this.pElementos.add(this.bNuevoJuego);
-		this.pElementos.add(this.lMejor_puntaje);
-		this.pElementos.add(this.lPuntos);
-		this.bNuevoJuego.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				nuevoJuego();
-			}
-		});
+    public Visual() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(700, 700);
+        this.setTitle("Snake");
+        this.setLayout(new BorderLayout());
 
-		this.add(this.pElementos, BorderLayout.NORTH);
+        // Panel de elementos
+        this.bNuevoJuego = new JButton("Nuevo Juego");
+        this.lMejor_puntaje = new JLabel("Mejor puntaje: 0");
+        this.lPuntos = new JLabel("Puntos: 0");
+        this.pElementos = new JPanel();
+        this.pElementos.setLayout(new GridLayout(1, 3, 10, 10));
+        this.pElementos.add(this.bNuevoJuego);
+        this.pElementos.add(this.lMejor_puntaje);
+        this.pElementos.add(this.lPuntos);
+        this.bNuevoJuego.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                nuevoJuego();
+            }
+        });
 
-		// Panel de la cuadricula del juego
-		this.movimiento = new Movimiento(500, 16);
-		this.pCuadricula2 = new PCuadricula2(550, 16, movimiento);
-		this.pCuadricula = new PCuadricula(550, 16);
+        this.add(this.pElementos, BorderLayout.NORTH);
 
-		// Panel de fondo
-		this.pFondo = new JPanel(null);
-		this.pFondo.add(this.pCuadricula2);
-		this.pFondo.add(this.pCuadricula);
+        // Panel de la cuadricula del juego
+        this.pCuadricula2 = new PCuadricula2(550, 16);
+        this.pCuadricula = new PCuadricula(550, 16);
 
-		this.pCuadricula2.setBounds(10, 10, 600, 600);
-		this.pCuadricula2.setOpaque(false);
-		this.pCuadricula.setBounds(10, 10, 600, 600);
-		
-		this.pCuadricula2.addKeyListener(new KeyListener() {
-			@Override
-			public void keyTyped(KeyEvent e) {
-			}
+        // Panel de fondo
+        this.pFondo = new JPanel(null);
+        this.pFondo.add(this.pCuadricula2);
+        this.pFondo.add(this.pCuadricula);
 
-			@Override
-			public void keyReleased(KeyEvent e) {
-			}
+        this.pCuadricula2.setBounds(10, 10, 600, 600);
+        this.pCuadricula2.setOpaque(false);
+        this.pCuadricula.setBounds(10, 10, 600, 600);
 
-			@Override
-			public void keyPressed(KeyEvent e) {
-				try {
-					teclaOprimida(e);
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		});
+        this.add(pFondo, BorderLayout.CENTER);
 
-		this.add(pFondo, BorderLayout.CENTER);
+        this.setVisible(true);
 
-		this.setVisible(true);
-		this.pCuadricula.setFocusable(true);
-		this.pCuadricula.requestFocusInWindow();
-	}
+        // Iniciar el bucle de actualización
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(100);
+                    pCuadricula2.actualizar();
+                    lPuntos.setText("Puntos: " + pCuadricula2.getPuntos());
+                    
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
 
-	protected void teclaOprimida(KeyEvent e) throws Exception {
-		if(e.getKeyCode() == DER) {
-			movimiento.cambioDireccion("Der");
-		}else if(e.getKeyCode() == IZQ) {
-			movimiento.cambioDireccion("Izq");
-		}else if(e.getKeyCode() == ARR) {
-			movimiento.cambioDireccion("Arr");
-		}else if(e.getKeyCode() == ABA) {
-			movimiento.cambioDireccion("aba");
-		}
-		movimiento.mover();
-		pCuadricula2.repaint();
-	}
+    public void nuevoJuego() {
+        this.pCuadricula2 = new PCuadricula2(550, 16);
+        this.pFondo.removeAll();
+        this.pFondo.add(this.pCuadricula2);
+        this.pFondo.add(this.pCuadricula);
 
-	protected void nuevoJuego() {
-		this.movimiento = new Movimiento(500, 16);
-		this.pCuadricula2 = new PCuadricula2(550, 16, movimiento);
-		this.pCuadricula2.setFocusable(true);
-		this.pCuadricula2.requestFocusInWindow();
+        this.pCuadricula2.setBounds(10, 10, 600, 600);
+        this.pCuadricula2.setOpaque(false);
+        this.pCuadricula.setBounds(10, 10, 600, 600);
+        
+        lMejor_puntaje.setText("Mejor puntaje: " + pCuadricula2.getBpuntos());
 
-		// Limpiar el panel de fondo y agregar las nuevas instancias
-		this.pFondo.removeAll();
-		this.pFondo.add(this.pCuadricula2);
-		this.pFondo.add(this.pCuadricula);
+        this.pFondo.revalidate();
+        this.pFondo.repaint();
+        
+        this.pCuadricula2.requestFocusInWindow(); // Asegurarse de que el nuevo juego tenga el foco del teclado
+    }
 
-		this.pCuadricula2.setBounds(10, 10, 600, 600);
-		this.pCuadricula2.setOpaque(false);
-		this.pCuadricula.setBounds(10, 10, 600, 600);
-
-		this.pFondo.revalidate();
-		this.pFondo.repaint();
-	}
-
-	public static void main(String[] args) {
-		new Visual();
-	}
+    public static void main(String[] args) {
+        new Visual();
+    }
 }
